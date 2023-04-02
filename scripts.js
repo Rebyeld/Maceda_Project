942d563ebffee8d18302132f0cd3d7bc3ceb1143
Rebyeld <127067605+Rebyeld@users.noreply.github.com>
2023-04-02 23:34:05 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var uname;
+var pword;
+
+document.getElementById('username').innerHTML = uname;
+document.getElementById('password').innerHTML = pword;
+ 
+//if (uname=="srryforbadsbmissn" && pwword=="hadtocram") {
+    ///link: https://web.facebook.com/AxolotlLorax/?_rdc=1&_rdr;
+//}
+
+//HI SIR
+//If you ead this sorry for the bad submission, im cramming to the max for exams and its 11:30 pm on sunday right now as im making this
+//Ill do better next quarter so sorry for now
